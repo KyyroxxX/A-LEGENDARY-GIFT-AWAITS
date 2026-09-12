9df08508e5d3bfc2733402c3aacfdf66d9d240b5
Kyrox <150089003+KyyroxxX@users.noreply.github.com>
2026-09-12 16:35:51 +0200

    Use correct staged model directory

--- js/game/staged-sprites.js
+++ js/game/staged-sprites.js
@@ -6,7 +6,7 @@
  * multiple characters into a "collage". Use one URL only.
  */
 const StagedSprites = {
-    BASE: 'assets/sprites/src/user_refs/staged',
+    BASE: 'assets/sprites/src/user_refs/_staged',
     _ver: null,
     _files: null,
     _facesLeft: null,
